e3486febc96fca6f8750005778092f71624d6808
swapnilsaahu <swapnilvp846@gmail.com>
2025-03-19 00:56:56 +0530

    added new battle page and some css

---
+++ src/battlePoke.js
@@ -0,0 +1,15 @@
+const selectedPokemon = localStorage.getItem("selectedPokemon");
+document.getElementById("displaySelectedPokemon").textContent = selectedPokemon;
+displayPokemon();
+async function displayPokemon(){
+    try{
+        const displayPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`)
+        const getPokeJson = await displayPokemon.json();
+        console.log(getPokeJson)
+        const displayPlayer=document.getElementById("playerImagePokemon");
+        displayPlayer.src=getPokeJson.sprites.front_default;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
